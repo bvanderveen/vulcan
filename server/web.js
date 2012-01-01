3288f17751bfd68d8b869ef5175eaa96a2ffbe40
Benjamin van der Veen <b@bvanderveen.com>
2012-01-01 13:52:44 -0800

    more verbose error handling in the server

--- server/web.js
+++ server/web.js
@@ -6,10 +6,12 @@ var fs      = require('fs');
 var spawner = require('spawner').create();
 var sys     = require('sys');
 var uuid    = require('node-uuid');
+var connect = require('connect');
 
 var app = express.createServer(
   express.logger(),
   express.cookieParser(),
+  express.errorHandler({ showStack: true }),
   express.session({ secret: process.env.SECRET }),
   require('connect-form')({ keepExtensions: true })
 );
@@ -22,25 +24,51 @@ var couchdb_options = couchdb_url.auth ?
 var db = new(cradle.Connection)(couchdb_url.hostname, couchdb_url.port || 5984, couchdb_options).database('make');
 db.create();
 
+var writeError = function (msg, response, err) {
+  response.writeHead(500);
+
+  if (msg) {
+    response.write(msg + '\n');
+  }
+
+  if (err) {
+    response.write('Error occurred: ');
+    if (err.name)
+      response.write('[' + err.name + '] ');
+    
+    if (err.message)
+      response.write(err.message + '\n');
+    else if (err.error)
+      response.write(err.error + '\n');
+    else {
+      var dump = '';
+      for (property in object) {
+        dump += property + ': ' + object[property]+'; ';
+      }
+      response.write("Dump of error object: '" + dump + "']\n");
+    }
+  }
+}
+
 // POST /make starts a build
 app.post('/make', function(request, response, next) {
 
   // require a form
   if (! request.form) {
-    response.write('invalid form');
-    response.send(500);
+    writeError("did not find form data", response);
   } else {
 
     // form handler
     request.form.complete(function(err, fields, files) {
 
-      // if there's an error, pass through to the next handler
-      if (err) { return next(err); }
+      // if there's an error, dump it
+      if (err) { 
+        writeError("error parsing form", response, err);
+      }
 
       // match on the shared secret
       if (fields.secret != process.env.SECRET) {
-        response.write('invalid secret');
-        response.send(500);
+        writeError("invalid secret", response);
       } else {
 
         var id      = uuid();
@@ -49,7 +77,10 @@ app.post('/make', function(request, response, next) {
 
         // create a couchdb documents for this build
         db.save(id, { command:command, prefix:prefix }, function(err, doc) {
-          if (err) { return next(err); }
+          if (err) { 
+            writeError("error saving doc to couch", response, err);
+            return;
+          }
 
           // save the input tarball as an attachment
           db.saveAttachment(
@@ -59,17 +90,18 @@ app.post('/make', function(request, response, next) {
             'application/octet-stream',
             fs.createReadStream(files.code.path),
             function(err, data) {
-              if (err) { return next(err); }
+              if (err) {
+                writeError("error saving input tarball to couch", response, err);
+                return;
+              }
 
               // spawn bin/make with this build id
               var ls = spawner.spawn('bin/make ' + id, function(err) {
-                response.write('could not spawn: ' + err);
-                response.send(500);
+                writeError("could not spawn", response, err);
               });
 
-              ls.on('error', function(error) {
-                response.write('error: ' + err);
-                response.send(500);
+              ls.on('error', function(err) {
+                writeError("error from spawner", response, err);
               });
 
               ls.on('data', function(data) {
